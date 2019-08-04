128c109811b9055175ce27312672f862485f8ec6
bodharth1004 <lbodharth@yahoo.in>
2019-08-04 12:14:06 +0530

    sunspot gem added

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,15 @@ class Product < ApplicationRecord
   has_many :order_items
   belongs_to :category
 
+  searchable do
+    text :name, :base_price, :image
+    time :created_at
+    string :base_price
+  end
+
   mount_uploader :image, ProductUploader
+
+  def base_price
+    price.to_i
+  end
 end
